396c62f5d0d57d873ea614c0338b672040b3f99e
Rebecca Finne <rebecca.finne@polestar.com>
2021-12-01 10:00:41 +0100

    refactor day1 pt 2

--- Day1/pt2.js
+++ Day1/pt2.js
@@ -5,21 +5,25 @@ fs.readFile("Day1/input1.txt", function (err, data) {
     if (err)
         throw err;
     var arr = data.toString().replace(/\r\n/g, "\n").split("\n");
-    var newSums = [];
     var largerMeasurementsSums = 0;
     var prevMeasurement = 10000;
+    //   for (let i = 0; i < arr.length; i++) {
+    //     if (arr[i + 2]) {
+    //       newSums.push(
+    //         parseInt(arr[i]) + parseInt(arr[i + 1]) + parseInt(arr[i + 2])
+    //       );
+    //     }
+    //   }
     for (var i = 0; i < arr.length; i++) {
         if (arr[i + 2]) {
-            newSums.push(parseInt(arr[i]) + parseInt(arr[i + 1]) + parseInt(arr[i + 2]));
-        }
-    }
-    for (var i = 0; i < newSums.length; i++) {
-        if (newSums[i] > prevMeasurement) {
-            largerMeasurementsSums += 1;
-            prevMeasurement = newSums[i];
-        }
-        else {
-            prevMeasurement = newSums[i];
+            var newComparison = parseInt(arr[i]) + parseInt(arr[i + 1]) + parseInt(arr[i + 2]);
+            if (newComparison > prevMeasurement) {
+                largerMeasurementsSums += 1;
+                prevMeasurement = newComparison;
+            }
+            else {
+                prevMeasurement = newComparison;
+            }
         }
     }
     console.log(largerMeasurementsSums);
--- Day1/pt2.ts
+++ Day1/pt2.ts
@@ -4,24 +4,19 @@ fs.readFile("Day1/input1.txt", function (err, data) {
   if (err) throw err;
 
   const arr = data.toString().replace(/\r\n/g, "\n").split("\n");
-  let newSums = [];
   let largerMeasurementsSums = 0;
   let prevMeasurement = 10000;
 
   for (let i = 0; i < arr.length; i++) {
     if (arr[i + 2]) {
-      newSums.push(
-        parseInt(arr[i]) + parseInt(arr[i + 1]) + parseInt(arr[i + 2])
-      );
-    }
-  }
-
-  for (let i = 0; i < newSums.length; i++) {
-    if (newSums[i] > prevMeasurement) {
-      largerMeasurementsSums += 1;
-      prevMeasurement = newSums[i];
-    } else {
-      prevMeasurement = newSums[i];
+      const newComparison =
+        parseInt(arr[i]) + parseInt(arr[i + 1]) + parseInt(arr[i + 2]);
+      if (newComparison > prevMeasurement) {
+        largerMeasurementsSums += 1;
+        prevMeasurement = newComparison;
+      } else {
+        prevMeasurement = newComparison;
+      }
     }
   }
   console.log(largerMeasurementsSums);
